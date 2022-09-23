9b26c328bade7d62e9e390d690380c7a3680c03d
Dwi Fitriawati Fajrin <99872829+dwifajrin17@users.noreply.github.com>
2022-09-23 18:50:15 +0700

    Add files via upload

---
+++ 19092022_Data Type.py
@@ -0,0 +1,321 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[2]:
+
+
+a = 10
+
+
+# In[3]:
+
+
+type(a)
+
+
+# In[4]:
+
+
+b = 1.7
+
+
+# In[5]:
+
+
+type(b)
+
+
+# In[6]:
+
+
+a = 1
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[8]:
+
+
+b = 1.0
+
+
+# In[9]:
+
+
+type(b)
+
+
+# In[10]:
+
+
+c = 1+2j
+
+
+# In[11]:
+
+
+type(c)
+
+
+# In[12]:
+
+
+a = 123
+
+
+# In[13]:
+
+
+type(a)
+
+
+# In[14]:
+
+
+b = 123.32
+
+
+# In[15]:
+
+
+type(b)
+
+
+# In[16]:
+
+
+c = 123.3+3.3j
+
+
+# In[17]:
+
+
+type(c)
+
+
+# test 123 123 
+
+# In[18]:
+
+
+a = 123
+b = 123.32
+c = 123.3+3.3j
+
+
+# In[19]:
+
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+# In[20]:
+
+
+a = 123
+b = 123.32
+c = 123.3+3.3j
+
+
+# In[21]:
+
+
+print(type a)
+print(type b)
+print(type c)
+
+
+# In[41]:
+
+
+a = 'september'
+b = '2022'
+
+
+# In[42]:
+
+
+print(a)
+print(b)
+
+
+# In[24]:
+
+
+type(a)
+
+
+# In[25]:
+
+
+a = "September"
+
+
+# In[26]:
+
+
+type(a)
+
+
+# In[27]:
+
+
+print(a)
+
+
+# In[28]:
+
+
+a = """Bootcamp Data Science.
+Kampus Merdeka X MyEduSolve.
+Kelas B."""
+
+
+# In[29]:
+
+
+print(a)
+
+
+# \n = for new line
+
+# In[30]:
+
+
+"Bootcamp \n Data Science"
+
+
+# In[33]:
+
+
+a = "Bootcamp \nData Science"
+
+
+# In[32]:
+
+
+print(a)
+
+
+# In[34]:
+
+
+print(a)
+
+
+# In[ ]:
+
+
+\ = for escape character
+
+
+# In[35]:
+
+
+'I\'am'
+
+
+# In[ ]:
+
+
+boolean = true false
+
+
+# In[38]:
+
+
+2 = 5
+
+
+# In[39]:
+
+
+2 > 5
+
+
+# In[40]:
+
+
+2 <= 2
+
+
+# In[43]:
+
+
+a = 'Dwi Fitriawati Fajrin'
+
+
+# In[44]:
+
+
+type(a)
+
+
+# In[45]:
+
+
+print(a)
+
+
+# In[46]:
+
+
+b = 'Dwi \nFitriawati \nFajrin'
+
+
+# In[48]:
+
+
+print(b)
+
+
+# In[49]:
+
+
+c = 'Dwi \nUPI \nLampung'
+
+
+# In[50]:
+
+
+print(c)
+
+
+# In[51]:
+
+
+33.3 > 30.0
+
+
+# In[52]:
+
+
+a = 'Dwi Fitriawati Fajrin'
+c = 'Dwi \nUPI \nLampung'
+23 !=5
+
+
+# In[53]:
+
+
+print(a)
+print(c)
+
+
+# In[ ]:
+
+
+
+
